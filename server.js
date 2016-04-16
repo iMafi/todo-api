7ccf3981654d5726be1987f1e4f744b9f0516567
Dmitriy Osetrov <dosetrov@yahoo.com>
2016-04-16 16:45:16 +0300

    Add PUT todos

--- server.js
+++ server.js
@@ -58,6 +58,34 @@ app.delete('/todos/:id', function(req, res) {
     }
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    var todoId = parseInt(req.params.id, 10);
+    var todo = _.findWhere(todos, {id: todoId});
+
+    if (!todo) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim()) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    _.extend(todo, validAttributes);
+    res.json(todo);
+
+});
+
 app.listen(PORT, function() {
     console.log('Express Listening on PORT: ' + PORT);
 });
