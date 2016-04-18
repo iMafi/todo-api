7af31e47ea3c8fb8fdb6382d3fe110912360cc7b
Dmitriy Osetrov <dosetrov@yahoo.com>
2016-04-18 23:51:53 +0300

    Added sqlite&sequelize to POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var todos = [];
@@ -52,14 +53,20 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || !body.description.trim()) {
+    db.todo.create(body).then(function(data) {
+        res.json(data);
+    }).catch(function(err) {
+        res.status(400).json(err);
+    });
+
+    /*if (!_.isBoolean(body.completed) || !_.isString(body.description) || !body.description.trim()) {
         return res.status(400).send();
     }
 
     body.description = body.description.trim();
     body.id = todoNextId++;
     todos[todos.length] = body;
-    res.json(body);
+    res.json(body);*/
 });
 
 // DELETE /todos/:id
@@ -103,6 +110,8 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-app.listen(PORT, function() {
-    console.log('Express Listening on PORT: ' + PORT);
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log('Express Listening on PORT: ' + PORT);
+    });
 });
