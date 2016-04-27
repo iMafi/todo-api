3f9e7067f654b916acdf48a160c47faa472fd95b
Dmitriy Osetrov <dosetrov@yahoo.com>
2016-04-27 22:54:09 +0300

    Added force:true option to sequelize call

--- server.js
+++ server.js
@@ -160,7 +160,7 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
     });
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
     app.listen(PORT, function() {
         console.log('Express Listening on PORT: ' + PORT);
     });
